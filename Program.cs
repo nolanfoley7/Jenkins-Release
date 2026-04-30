1b0c0964c54b90299a0d405766c24ecf65f081dd
Neal, Vadin <vadin.neal@okstate.edu>
2026-04-29 21:51:05 -0500

    added more raygun changes

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using Mindscape.Raygun4Net.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddRaygun(builder.Configuration);
 
 var app = builder.Build();
 
@@ -9,17 +12,14 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+app.UseRaygun();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
+app.MapGet("/throw", (Func<string>)(() => throw new Exception("Exception in request pipeline")));
 app.Run();
